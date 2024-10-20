1e86d67e6143ff1a052d05775f6f41a4e9ffb7e6
sndmaa <sanclaw55@gmail.com>
2024-10-21 02:31:16 +0400

    advanced error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,9 @@
 use super::method::Method;
+use core::str;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Display, Debug, Formatter, Result as FmtResult};    // Errors have to apply both the Debug and Display Traits
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -18,8 +20,21 @@ impl TryFrom<&[u8]> for Request {
 
     // GET /search?name=abc&sort=1 HTTP/1.1 - example request
 
-    fn try_from(value: &[u8]) -> Result<Self, Self::Error>{
-        unimplemented!()
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{
+        // match str::from_utf8(buf) {
+        //     Ok(request) => {}
+        //     Err(_) => return Err(ParseError::InvalidEncoding)
+        // }
+
+        // Above error handling comment can be written alternatively as:
+        // match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {    // This returns the res:Err if the result is an error if nto it returns the Ok value of self
+        //     Ok(request) => {}
+        //     Err(e) => Err(e)
+        // }
+
+        // This in turn can be written in shorthand as the following. Only difference is this will try to convert the error type if it does not match the initally specified type:
+        let request = str::from_utf8(buf)?;
+
     }
 }
 
@@ -40,6 +55,12 @@ impl ParseError {
             Self::InvalidMethod => "Invalid Method",
             
         }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(value: T) -> Self {
+        Self::InvalidEncoding
     }
 }
 
