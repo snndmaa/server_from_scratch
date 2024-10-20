587fae319458833129276be33caec080f7d998c7
sndmaa <sanclaw55@gmail.com>
2024-10-21 00:13:33 +0400

    traits and type conversions

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,20 @@
 use super::method::Method;
+use std::convert::TryFrom;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method,
+}
+
+impl Request {
+    // fn from_byte_array(buf: &[u8]) -> Result<Self, String> {}   // We don't do it this way because using traits is idiomatic rust and how type conversions are meant to be carried out.
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error>{
+        unimplemented!()
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::io::Read;
 use std::net::TcpListener;
+use std::convert::TryFrom;  // Before we can call functions from traits we have to pull the trait into our scope
+use std::convert::TryInto;  // TryFrom implements TryInto therefore we must call this
+use crate::http::Request;   // Accessing the module from the import specified in our root(main.rs)
 pub struct Server {
     addr: String,
 }
@@ -22,7 +25,15 @@ impl Server {
                     let mut buffer = [0; 1024]; // Dynamically allocate buffer
                     match stream.read(&mut buffer) { // Read data into the buffer    // read requires mutable reference to self as well as &mut [u8]. therefore both array and vector can be used however if an array is used this can cause a buffer overflow if the data received on the socket is more than the specified size.
                         Ok(_) => {
-                            println!("Received a Request: {}", String::from_utf8_lossy(&buffer))  // We use this safe version of not checking if the utf8 is valid because we dont want the app to crash even if the ut8 isn't valid just yet, there's validation for this ahead
+                            println!("Received a Request: {}", String::from_utf8_lossy(&buffer));  // We use this safe version of not checking if the utf8 is valid because we dont want the app to crash even if the ut8 isn't valid just yet, there's validation for this ahead
+                            
+                            // let res: &Result<Request, _> = &buffer[..].try_into();   // Alternative way to call our conversion function using try_into instead of try_from
+                            match Request::try_from(&buffer[..]){    // We convert this reference to an array of bytes &[u8; 1024] into a byte slice &[u8] to satisfy our trait function. Another way to write this would have been (&buffer as &[u8])
+                                Ok(request) => {
+                                    
+                                }
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e)
                     }
