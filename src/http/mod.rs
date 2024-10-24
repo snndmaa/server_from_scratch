ccd9d92648e53a8db76b74cb538ceb758db9976a
sndmaa <sanclaw55@gmail.com>
2024-10-25 01:22:52 +0400

    response model

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,7 +1,9 @@
 pub use method::Method;
-pub use request::{Request, ParseError};
-pub use query_string::{QueryString, Value as QueryStringValue};
+pub use request::{ Request, ParseError };
+pub use query_string::{ QueryString, Value as QueryStringValue };
+pub use response::{ Response, StatusCode };
 
 pub mod request;
 pub mod method;
 pub mod query_string;
+pub mod response;
